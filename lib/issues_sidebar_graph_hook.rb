897d03adb2731b74515162618a9cc01cf384fd27
mashit <hi_2smart@yahoo.co.in>
2013-12-31 19:06:56 +0530

    corrected the relative paths of the resources.

--- lib/issues_sidebar_graph_hook.rb
+++ lib/issues_sidebar_graph_hook.rb
@@ -3,20 +3,20 @@ class IssuesSidebarGraphHook < Redmine::Hook::ViewListener
   def view_issues_sidebar_issues_bottom(context = { })
     if context[:project].nil?
       	output = "<h3>#{l(:label_graphs)}</h3>"
-      	output << link_to(l(:label_graphs_old_issues), {:controller => 'graphs', :action => 'old_issues', :only_path => true})
+      	output << link_to(l(:label_graphs_old_issues), {:controller => 'graphs', :action => 'old_issues', :host => Setting.host_name})
       	output << "<br/>"
-        output << link_to(l(:label_graphs_issue_growth), {:controller => 'graphs', :action => 'issue_growth', :only_path => true})
+        output << link_to(l(:label_graphs_issue_growth), {:controller => 'graphs', :action => 'issue_growth', :host => Setting.host_name})
         output << "<br/>"
-	 output << link_to(l(:label_graphs_bug_growth), {:controller => 'graphs', :action => 'bug_growth', :only_path => true})
+	 output << link_to(l(:label_graphs_bug_growth), {:controller => 'graphs', :action => 'bug_growth', :host => Setting.host_name})
         output << "<br/>"
         return output
   	elsif !context[:project].nil?
         output = "<h3>#{l(:label_graphs)}</h3>"
-        output << link_to(l(:label_graphs_old_issues), {:controller => 'graphs', :action => 'old_issues', :project_id => context[:project], :only_path => true})
+        output << link_to(l(:label_graphs_old_issues), {:controller => 'graphs', :action => 'old_issues', :project_id => context[:project], :host => Setting.host_name})
         output << "<br/>"
-        output << link_to(l(:label_graphs_issue_growth), {:controller => 'graphs', :action => 'issue_growth', :project_id => context[:project], :only_path => true})
+        output << link_to(l(:label_graphs_issue_growth), {:controller => 'graphs', :action => 'issue_growth', :project_id => context[:project], :host => Setting.host_name})
         output << "<br/>"
-	 output << link_to(l(:label_graphs_bug_growth), {:controller => 'graphs', :action => 'bug_growth', :project_id => context[:project], :only_path => true})
+	 output << link_to(l(:label_graphs_bug_growth), {:controller => 'graphs', :action => 'bug_growth', :project_id => context[:project], :host => Setting.host_name})
         output << "<br/>"
         return output
     end
